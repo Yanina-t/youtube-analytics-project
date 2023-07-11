542a9bf97a8f357fbcf88be24dbf2df55dffdf72
Yanina-t <vyaninat@gmail.com>
2023-07-11 19:25:29 +0200

    add inform YT

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import json
+
+from googleapiclient.discovery import build
+
+from helper.youtube_api_manual import api_key
 
 
 class Channel:
@@ -5,8 +10,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
