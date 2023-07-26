c61630dc1b57cc18d53dc7d0eaedc2050d08ff2f
Yanina-t <vyaninat@gmail.com>
2023-07-26 17:06:01 +0200

    add except IndexError in __init__ HW_6 YT

--- src/video.py
+++ src/video.py
@@ -8,13 +8,20 @@ class Video(YTMixin):
         Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
         """
         self.video = self.get_service().videos().list(id=video_id, part='snippet,contentDetails,statistics').execute()
-        self.title: str = self.video['items'][0]['snippet']['title']  # название видео
-        self.url = f'https://www.youtube.com/video/{video_id}'  # ссылка на видео
-        self.view_count: str = self.video['items'][0]['statistics']['viewCount']  # количество просмотров
-        self.like_count: str = self.video['items'][0]['statistics']['likeCount']  # количество лайков
+        try:
+            self.title: str = self.video['items'][0]['snippet']['title']  # название видео
+            self.url = f'https://www.youtube.com/video/{video_id}'  # ссылка на видео
+            self.view_count: str = self.video['items'][0]['statistics']['viewCount']  # количество просмотров
+            self.like_count: str = self.video['items'][0]['statistics']['likeCount']  # количество лайков
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f"{self.title}"
+
 
 class PLVideo(Video):
     def __init__(self, video_id: str, play_list_id: str):
